1689755cfc560d8a8b34cfaea7b425bf2955b630
tanner0101 <me@tanner.xyz>
2017-05-16 17:36:11 +0100

    vapor 2

--- generate.sh
+++ generate.sh
@@ -1,26 +1,47 @@
 cd code
 
-git pull
-vapor clean --xcode
-vapor xcode -n
-
-mv README.md README.md.bu
+rm -rf vapor
+git clone git@github.com:vapor/vapor.git
+cd vapor 
+swift package generate-xcodeproj
 
 echo "# API Documentation" > README.md
 echo "View API documentation for Vapor's modules by choosing one of the modules from the top, then selecting a documented component from the left hand column." >> README.md
 
-jazzy --xcodebuild-arguments -scheme,Vapor --module Auth --output ../auth xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module Cache --output ../cache xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module Cookies --output ../cookies xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module Sessions --output ../sessions xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module Vapor --output ../vapor xcodebuild
+jazzy -x -scheme,Vapor --module Cache --output ../../cache
+jazzy -x -scheme,Vapor --module Configs --output ../../configs
+jazzy -x -scheme,Vapor --module Testing --output ../../testing
+jazzy -x -scheme,Vapor --module Sessions --output ../../sessions
+jazzy -x -scheme,Vapor --module Vapor --output ../../vapor
+jazzy -x -scheme,Vapor --module Bits --output ../../bits
+jazzy -x -scheme,Vapor --module Debugging --output ../../debugging
+jazzy -x -scheme,Vapor --module Core --output ../../core
+jazzy -x -scheme,Vapor --module Crypto --output ../../crypto
+jazzy -x -scheme,Vapor --module Node --output ../../node
+jazzy -x -scheme,Vapor --module PathIndexable --output ../../pathindexable
+jazzy -x -scheme,Vapor --module JSON --output ../../json
+jazzy -x -scheme,Vapor --module Branches --output ../../branches
+jazzy -x -scheme,Vapor --module Routing --output ../../routing
+jazzy -x -scheme,Vapor --module Multipart --output ../../multipart
+jazzy -x -scheme,Vapor --module FormData --output ../../formdata
+jazzy -x -scheme,Vapor --module BCrypt --output ../../bcrypt
+jazzy -x -scheme,Vapor --module WebSockets --output ../../websockets
+jazzy -x -scheme,Vapor --module SMTP --output ../../smtp
+jazzy -x -scheme,Vapor --module HTTP --output ../../http
+jazzy -x -scheme,Vapor --module URI --output ../../uri
+jazzy -x -scheme,Vapor --module Transport --output ../../transport
+jazzy -x -scheme,Vapor --module Sockets --output ../../sockets
 
-jazzy --xcodebuild-arguments -scheme,Vapor --module WebSockets --output ../websockets xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module SMTP --output ../smtp xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module HTTP --output ../http xcodebuild
-jazzy --xcodebuild-arguments -scheme,Vapor --module Transport --output ../transport xcodebuild
+cd ..
 
-jazzy --xcodebuild-arguments -scheme,Vapor --module Fluent --output ../fluent xcodebuild
+rm -rf fluent
+git clone git@github.com:vapor/fluent.git
+cd fluent 
+swift package generate-xcodeproj
 
-rm README.md
-mv README.md.bu README.md
+echo "# API Documentation" > README.md
+echo "View API documentation for Vapor's modules by choosing one of the modules from the top, then selecting a documented component from the left hand column." >> README.md
+
+jazzy -x -scheme,Fluent --module Fluent --output ../../fluent
+
+cd ../../
