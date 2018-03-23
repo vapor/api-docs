5bb1c509edeb6c1ebbae755ead041f6409052f35
tanner0101 <me@tanner.xyz>
2018-03-23 16:35:12 -0400

    3.0 updates

--- generate.sh
+++ generate.sh
@@ -1,47 +1,38 @@
-cd code
+function generate_docs() {
+	REPO_NAME=$1
+	MODULE=$2
+	VERSION=$3
 
-rm -rf vapor
-git clone git@github.com:vapor/vapor.git
-cd vapor 
-swift package generate-xcodeproj
+	# create the unique docs path
+	DOCS_PATH=$REPO_NAME/$VERSION/$MODULE
 
-echo "# API Documentation" > README.md
-echo "View API documentation for Vapor's modules by choosing one of the modules from the top, then selecting a documented component from the left hand column." >> README.md
+	echo "💧  Generating docs for $DOCS_PATH"
 
-jazzy -x -scheme,Vapor --module Cache --output ../../cache
-jazzy -x -scheme,Vapor --module Configs --output ../../configs
-jazzy -x -scheme,Vapor --module Testing --output ../../testing
-jazzy -x -scheme,Vapor --module Sessions --output ../../sessions
-jazzy -x -scheme,Vapor --module Vapor --output ../../vapor
-jazzy -x -scheme,Vapor --module Bits --output ../../bits
-jazzy -x -scheme,Vapor --module Debugging --output ../../debugging
-jazzy -x -scheme,Vapor --module Core --output ../../core
-jazzy -x -scheme,Vapor --module Crypto --output ../../crypto
-jazzy -x -scheme,Vapor --module Node --output ../../node
-jazzy -x -scheme,Vapor --module PathIndexable --output ../../pathindexable
-jazzy -x -scheme,Vapor --module JSON --output ../../json
-jazzy -x -scheme,Vapor --module Branches --output ../../branches
-jazzy -x -scheme,Vapor --module Routing --output ../../routing
-jazzy -x -scheme,Vapor --module Multipart --output ../../multipart
-jazzy -x -scheme,Vapor --module FormData --output ../../formdata
-jazzy -x -scheme,Vapor --module BCrypt --output ../../bcrypt
-jazzy -x -scheme,Vapor --module WebSockets --output ../../websockets
-jazzy -x -scheme,Vapor --module SMTP --output ../../smtp
-jazzy -x -scheme,Vapor --module HTTP --output ../../http
-jazzy -x -scheme,Vapor --module URI --output ../../uri
-jazzy -x -scheme,Vapor --module Transport --output ../../transport
-jazzy -x -scheme,Vapor --module Sockets --output ../../sockets
+	# remove these docs if they already exist
+	rm -rf $DOCS_PATH
 
-cd ..
+	mkdir code
+	cd code
+	git clone -b $VERSION git@github.com:vapor/$REPO_NAME.git
+	cd $REPO_NAME
 
-rm -rf fluent
-git clone git@github.com:vapor/fluent.git
-cd fluent 
-swift package generate-xcodeproj
+	swift build
+	sourcekitten doc --spm-module $MODULE > module.json
 
-echo "# API Documentation" > README.md
-echo "View API documentation for Vapor's modules by choosing one of the modules from the top, then selecting a documented component from the left hand column." >> README.md
+	jazzy \
+		--clean \
+  		--sourcekitten-sourcefile module.json \
+		--author Vapor \
+		--author_url https://vapor.codes \
+		--github_url https://github.com/vapor/$REPO_NAME \
+		--github-file-prefix https://github.com/vapor/$REPO_NAME/tree/$VERSION \
+		--module-version $VERSION \
+		--module $MODULE \
+		--root-url https://api.vapor.codes/$DOCS_PATH \
+		--output ../../$DOCS_PATH
 
-jazzy -x -scheme,Fluent --module Fluent --output ../../fluent
+	cd ../../
+	rm -rf code
+}
 
-cd ../../
+generate_docs $1 $2 $3
