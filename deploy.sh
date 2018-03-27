594e77332dede90615683a2033c5d62e77bd6de5
tanner0101 <me@tanner.xyz>
2018-03-27 10:53:41 -0400

    build

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,33 @@
-O=""
+O="<h1>Directory Listing</h1>"
 
 for REPO in */
 do
 	cd $REPO
-	echo "REPO: $REPO"
+	O="$O$REPO"
+	O="$O<ul>"
 	for VERSION in */
 	do
 		cd $VERSION
-		echo "  VERSION: $VERSION"
+		O="$O<li>"
+		O="$O$VERSION"
+		O="$O<ul>"
 			for MODULE in */
 			do
 				cd $MODULE
-				echo "    MODULE: $MODULE"
-
+				O="$O<li>"
+				O="$O<a href=\"/$REPO$VERSION${MODULE}index.html\">$MODULE</a><br>"
+				O="$O</li>"
 				cd ..
 			done
+		O="$O</ul>"
+		O="$O</li>"
 		cd ..
 	done
+	O="$O</ul>"
 	cd ..
 done
 
+echo -e $O > index.html;
 
 git add .
 git commit -am "build"
