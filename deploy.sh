83f48afaf3c7fde6568ee656dc9dc36640cad818
tanner0101 <me@tanner.xyz>
2018-03-27 10:38:33 -0400

    build

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
+O=""
+
 for REPO in */
 do
 	cd $REPO
-	echo "$REPO"
+	echo "REPO: $REPO"
 	for VERSION in */
 	do
 		cd $VERSION
-		echo "$VERSION"
+		echo "  VERSION: $VERSION"
+			for MODULE in */
+			do
+				cd $MODULE
+				echo "    MODULE: $MODULE"
 
+				cd ..
+			done
 		cd ..
 	done
 	cd ..
