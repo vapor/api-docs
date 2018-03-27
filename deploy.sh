9704273a54a43fad5dd13d14d69f9344385d0629
tanner0101 <me@tanner.xyz>
2018-03-27 10:36:48 -0400

    build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
+for REPO in */
+do
+	cd $REPO
+	echo "$REPO"
+	for VERSION in */
+	do
+		cd $VERSION
+		echo "$VERSION"
+
+		cd ..
+	done
+	cd ..
+done
+
+
 git add .
 git commit -am "build"
 git push
