914d167bc1b6a3fdf633dd21b6c718cbe21f9710
Tanner Nelson <me@tanner.xyz>
2019-05-22 10:23:54 -0400

    update latest tags

--- generate.sh
+++ generate.sh
@@ -6,29 +6,32 @@ function generate_docs() {
     VERSION=$3
     ROOT=$4
 
-    # create the unique docs path
     DOCS_PATH=$REPO_NAME/$VERSION/$MODULE
-    LATEST_PATH=$REPO_NAME/latest/$MODULE
 
-    echo "💧  Generating docs for $DOCS_PATH"
-    echo "    in $ROOT/$DOCS_PATH"
+    echo "    🧩 $MODULE"
 
-    if [ -e "$ROOT/$DOCS_PATH/index.html" ]; then
-        echo "$VERSION already exists, skipping..."
+    if [[ $MODULE == "COperatingSystem" ]] || 
+       [[ $MODULE == "Boilerplate" ]]; then
+        echo "       skipping module..."
+    elif [ -e "$ROOT/$DOCS_PATH/index.html" ] && [ $VERSION != "master" ]; then
+        echo "       already exists..."
+    elif [ -e "Sources/$MODULE/include" ]; then
+        echo "       skipping C module..."
+    elif [ -e "Sources/$MODULE/main.swift" ]; then
+        echo "       skipping executable..."
     else 
-        echo "Starting $VERSION..."
+        echo "       building..."
+        swift build &> /dev/null
+
+        echo "       sourcekitten doc..."
+        sourcekitten doc --spm-module $MODULE > module.json 2> /dev/null
+
+        rm -rf $ROOT/$DOCS_PATH
         mkdir -p $ROOT/$DOCS_PATH
-        mkdir .build
-        cd .build
-        git clone -b $VERSION git@github.com:vapor/$REPO_NAME.git
-        cd $REPO_NAME
-
-        swift build
-        sourcekitten doc --spm-module $MODULE > module.json
-
+        echo "       jazzy gen..."
         jazzy \
             --clean \
-                --sourcekitten-sourcefile module.json \
+            --sourcekitten-sourcefile module.json \
             --author Vapor \
             --author_url https://vapor.codes \
             --github_url https://github.com/vapor/$REPO_NAME \
@@ -37,35 +40,71 @@ function generate_docs() {
             --module $MODULE \
             --root-url https://api.vapor.codes/$DOCS_PATH/ \
             --theme fullwidth \
-            --output $ROOT/$DOCS_PATH
+            --output $ROOT/$DOCS_PATH \
+            &> /dev/null
 
-        cd ../../
-        rm -rf .build
-
-        # copy into latest
-        rm -rf $ROOT/$LATEST_PATH
-        cp -R $ROOT/$DOCS_PATH $ROOT/$LATEST_PATH
+        echo "       done..."
     fi
 }
 
 function main() {
+    echo "💧 Vapor docs generator"
     ROOT=`pwd`
     declare -a arr=(
-        "vapor"
+        "auth"
+        "console"
+        "core"
+        "crypto"
+        "database-kit"
+        "fluent"
+        "fluent-postgresql"
+        "fluent-sqlite"
+        "http"
+        "jwt"
+        "leaf"
+        "multipart"
+        "mysql"
         "nio-postgres"
+        "postgresql"
+        "redis"
+        "routing"
+        "service"
+        "sql"
+        "sqlite"
+        "template-kit"
+        "url-encoded-form"
+        "validation"
+        "vapor"
+        "websocket"
     )
     rm -rf .build
     mkdir .build
     cd .build
     for REPO_NAME in "${arr[@]}"
     do
-        echo "💧  Generating docs for $REPO_NAME"
-        git clone git@github.com:vapor/$REPO_NAME.git
+        echo "📦 $REPO_NAME.git"
+        git clone git@github.com:vapor/$REPO_NAME.git &> /dev/null
         cd $REPO_NAME
+
+        # all versions
         for VERSION in $(git tag | tail -n 1) 
         do
-            generate_docs $REPO_NAME "NIOPostgres" $VERSION $ROOT
+            echo "  🏷  $VERSION"
+            git checkout $VERSION &> /dev/null
+            for MODULE in Sources/*
+            do
+                generate_docs $REPO_NAME `basename $MODULE` $VERSION $ROOT
+            done
         done
+
+        # master
+        git checkout master &> /dev/null
+        echo "  🏷  master"
+        for MODULE in Sources/*
+        do
+            generate_docs $REPO_NAME `basename $MODULE` "master" $ROOT
+        done
+
         cd ../
         rm -rf $REPO_NAME
     done
