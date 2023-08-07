9c4907fb5f08b0cb59873f3c6069a3402508edeb
Gwynne Raskind <gwynne@vapor.codes>
2023-08-07 06:13:25 -0500

    Actually fix the script

--- generate-api-docs.swift
+++ generate-api-docs.swift
@@ -44,22 +44,41 @@ let htmlMenu = packages.values.flatMap { $0 }
     .map { "<option value=\"\($0.lowercased())/documentation/\($0.lowercased())\">\($0)</option>" }
     .joined(separator: "\n")
 
-let publicDirUrl = URL(fileURLWithPath: "./public", isDirectory: true)
-try FileManager.default.removeItem(at: publicDirUrl)
-try FileManager.default.createDirectory(at: publicDirUrl, withIntermediateDirectories: true)
+do {
+    let publicDirUrl = URL(fileURLWithPath: "./public", isDirectory: true)
+    try FileManager.default.removeItemIfExists(at: publicDirUrl)
+    try FileManager.default.createDirectory(at: publicDirUrl, withIntermediateDirectories: true)
 
-var htmlIndex = try String(contentsOf: URL(fileURLWithPath: "./index.html", isDirectory: false), encoding: .utf8)
-htmlIndex.replace("{{Options}}", with: "\(htmlMenu)\n", maxReplacements: 1)
+    var htmlIndex = try String(contentsOf: URL(fileURLWithPath: "./index.html", isDirectory: false), encoding: .utf8)
+    htmlIndex.replace("{{Options}}", with: "\(htmlMenu)\n", maxReplacements: 1)
 
-try htmlIndex.write(to: publicDirUrl.appendingPathComponent("index.html", isDirectory: false), atomically: true, encoding: .utf8)
-try FileManager.copyItem(at: URL(fileURLWithPath: "./api-docs.png", isDirectory: false), into: publicDirUrl)
-try FileManager.copyItem(at: URL(fileURLWithPath: "./error.html", isDirectory: false), into: publicDirUrl)
+    try htmlIndex.write(to: publicDirUrl.appendingPathComponent("index.html", isDirectory: false), atomically: true, encoding: .utf8)
+    try FileManager.default.copyItem(at: URL(fileURLWithPath: "./api-docs.png", isDirectory: false), into: publicDirUrl)
+    try FileManager.default.copyItem(at: URL(fileURLWithPath: "./error.html", isDirectory: false), into: publicDirUrl)
+} catch let error as NSError {
+    print("❌  ERROR: \(String(reflecting: error)): \(error.userInfo)")
+    exit(1)
+}
+
+extension NSError {
+    func isCocoaError(_ code: CocoaError.Code) -> Bool {
+        self.domain == CocoaError.errorDomain && self.code == code.rawValue
+    }
+}
 
 extension FileManager {
+    func removeItemIfExists(at url: URL) throws {
+        do {
+            try self.removeItem(at: url)
+        } catch let error as NSError where error.isCocoaError(.fileNoSuchFile) {
+            // ignore
+        }
+    }
+
     func copyItem(at src: URL, into dst: URL) throws {
         assert(dst.hasDirectoryPath)
 
-        let dstItem = dst.appendingPathComponent(src.lastPathComponent, isDirectory: src.hasDirectoryPath)
+        let dstItem = dst.appendingPathComponent(src.lastPathComponent, isDirectory: dst.hasDirectoryPath)
         try self.copyItem(at: src, to: dstItem)
     }
 }
