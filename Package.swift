6a04f923fab7aa5561acf40287a9e728be5da1ea
Tim <0xtimc@gmail.com>
2026-07-15 15:50:03 +0100

    Pull in version of Kiln that has the fixes

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        // TEMPORARY local override to validate the topic-fragment / sidebar-home
-        // features; revert to github/brokenhandsio once released.
-        .package(path: "../../BrokenHands/kiln"),
+        .package(url: "https://github.com/brokenhandsio/kiln.git", from: "1.8.4"),
         .package(url: "https://github.com/vapor/design.git", from: "1.0.0-rc.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
     ],
