b17921a752de98874aea918375329a5dbdeb3892
Tim <0xtimc@gmail.com>
2026-07-17 16:11:31 +0100

    Package updates

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/brokenhandsio/kiln.git", from: "1.8.4"),
-        .package(url: "https://github.com/vapor/design.git", from: "1.0.0"),
+        .package(url: "https://github.com/brokenhandsio/kiln.git", from: "1.8.5"),
+        .package(url: "https://github.com/vapor/design.git", from: "1.0.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
     ],
     targets: [
